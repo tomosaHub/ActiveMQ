a0366d6a19161293bb74ee961a542a7955771e71
Mosa Letswalo <letswalomosa@gmail.com>
2025-04-01 13:10:37 +0200

    one-to-many

--- src/main/java/com/example/springbootactivemqexample/MessageConsumer.java
+++ src/main/java/com/example/springbootactivemqexample/MessageConsumer.java
@@ -1,11 +1,18 @@
 package com.example.springbootactivemqexample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(MessageConsumer.class);
+
     @JmsListener(destination = "New.message")
-    public void receiveMessage(Message message) {System.out.println("Received message: " + message.getText());}
+    public void receiveMessage(Message message)
+    {
+        log.info("Received message: {}",message.getText() );
+    }
 }
